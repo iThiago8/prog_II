9f33f211bde5360bd630e395616215c5345f6d2a
411730 <411730@VDA-LCOII-19168>
2025-09-01 22:00:34 -0300

    Codando o exercício 7

--- Aulas/aula_05/models/Carro.php
+++ Aulas/aula_05/models/Carro.php
@@ -1,5 +1,33 @@
 <?php
 
 class Carro {
+    public string $modelo;
+    private float $velocidade;
+
+    public function __construct($modelo, $velocidadeAtual) {
+        $this->modelo = $modelo;
+        $this->velocidade = $velocidadeAtual;
+    }
+
+    public function acelerar($velocidade) {
+        if (($this->velocidade + $velocidade) > 200) {
+            echo "A velocidade não pode passar de 200 km/h!";
+            return;
+        }
+
+        $this->velocidade =+ $velocidade;
+        return $this->velocidade;
+    }
+
+    public function frear($velocidade) {
+        if (($this->velocidade + $velocidade) > 200) {
+            echo "A velocidade não pode diminuir de 0 km/h!";
+            return;
+        }
+
+        $this->velocidade =- $velocidade;
+        return $this->velocidade;
+    }
+
 
 }
